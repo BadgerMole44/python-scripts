ad2af78a16502f1ef25bd8efa91f41b82262b97b
keyganfourcythe <forsythek88@gmail.com>
2024-08-25 14:14:19 -0400

    edited camelCase to snake_case

--- filename_cleaner.py
+++ filename_cleaner.py
@@ -87,7 +87,7 @@ class Directory:
         while i < len(content_name):
             if content_name[i].isupper():
                 if i - 1 > 0:
-                    if content_name[i - 1].isalpha():
+                    if content_name[i - 1] != " ":
                         out += "_" 
                 out += content_name[i].lower()
             elif content_name[i] == " ":
