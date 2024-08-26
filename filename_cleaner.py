69df0ca8ae7ce63d10f5cf29b3dbd3b040662c35
keyganfourcythe <forsythek88@gmail.com>
2024-08-25 20:54:07 -0400

    fixed some parsing stuff

--- filename_cleaner.py
+++ filename_cleaner.py
@@ -87,7 +87,7 @@ class Directory:
         while i < len(content_name):
             if content_name[i].isupper():
                 if i - 1 > 0:
-                    if content_name[i - 1].isalpha() and content_name[i - 1].islower():
+                    if content_name[i - 1].islower() and out[len(out) - 1] != "_":
                         out += "_" 
                 out += content_name[i].lower()
             elif content_name[i] == " ":
